a3e31d6e213e6702d8ac9be8464c25083d497cb8
Kevin Lu <kevie.lu8@gmail.com>
2026-01-22 14:12:00 -0800

    some minor changes

--- OpenBench/templatetags/mytags.py
+++ OpenBench/templatetags/mytags.py
@@ -75,6 +75,8 @@ def shortStatBlock(test):
     elif test.test_mode == 'SPRT':
         llr_line = 'LLR: %0.2f (%0.2f, %0.2f) [%0.2f, %0.2f]' % (
             test.currentllr, test.lowerllr, test.upperllr, test.elolower, test.eloupper)
+        lower, elo, upper = OpenBench.stats.Elo(test.results())
+        elo_line = 'Elo: %0.2f +- %0.2f (95%%)' % (elo, max(upper - elo, elo - lower))
         statlines = [llr_line, tri_line, penta_line] if test.use_penta else [llr_line, tri_line]
 
     elif test.test_mode == 'GAMES':
